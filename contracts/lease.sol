2e23f956de43997ef1812545f7bff1c0c91979c0
jmacharia <jmacharia@jmacharia.com>
2022-07-18 16:01:49 +0300

    Updated

--- contracts/lease.sol
+++ contracts/lease.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.4.17;
 
 contract Lease {
     struct Request {
+        string model;
+        uint manufacturingYear;
         string description;
         uint value;
         address recipient;
@@ -12,18 +14,21 @@ contract Lease {
     }
 
     Request[] public requests;
-    address public manager;
+    address public owner;
     uint public minimumContribution;
     mapping(address => bool) public endorsers;
+    uint public endorsersCount;
 
-    modifier restricted() {
-        require(msg.sender == manager);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
         _;
     }
 
 
+
     function Lease( uint minimum) public {
-        manager = msg.sender;
+        owner = msg.sender;
         minimumContribution = minimum;
     }
 
@@ -33,11 +38,14 @@ contract Lease {
         require(msg.value > minimumContribution);
 
         endorsers[msg.sender] = true;
+        endorsersCount++;
     }
 
 
-    function createRequest(string description, uint value, address recipient) public restricted {
+    function createRequest(string model, uint manufacturingYear, string description, uint value, address recipient) public onlyOwner {
         Request memory leaseRequest = Request({
+            model: model,
+            manufacturingYear: manufacturingYear,
             description: description,
             value: value,
             recipient: recipient,
@@ -48,14 +56,27 @@ contract Lease {
         requests.push(leaseRequest);
     }
 
-function endorseRequest( uint id ) public {
-    Request storage request = requests[id];
 
-    require(endorsers[msg.sender]);
-    require(!requests[id].endorsements[msg.sender]);
 
-    request.endorsements[msg.sender] = true;
-    request.endorsementCount++;
+    function endorseRequest( uint id ) public {
+        Request storage request = requests[id];
 
-}
+        require(endorsers[msg.sender]);
+        require(!requests[id].endorsements[msg.sender]);
+
+        request.endorsements[msg.sender] = true;
+        request.endorsementCount++;
+
+    }
+
+    function finalizeRequest(uint id) public onlyOwner{
+        Request storage request = requests[id];
+
+        require(request.endorsementCount > (endorsersCount/2));    
+        require(!request.complete);
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+   
 }
